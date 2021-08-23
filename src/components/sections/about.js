18a20c5b8518ccfc34a0053fcae468fb978e2651
Shubham Soni <46859348+ShubhamSoni09@users.noreply.github.com>
2021-08-23 15:06:36 +0530

    changes for index

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,21 +136,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Shubham and I enjoy creating things that live on the internet. My
+              interest in web development started back in first year of my college when I built few
+              web pages using HTML & CSS. Apart from this I've also worked with many machine
+              learning algorithms. This interest further grew when I dived deep into the world of
+              computer science and learned new technologies! I also love to solve problems with
+              time-efficient data structures and algorithms.
             </p>
 
             <p>
-              Fast-forward to today, and I've had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I've worked as a Web Developer Intern at{' '}
+              <a href="https://redwalltechnologies.com/">a lean consulting startup</a>, and as a
+              Computer Vision Intern at{' '}
+              <a href="https://thesparksfoundation.com/">a deep tech company</a>, as a GRIP student.
             </p>
 
             <p>Here are a few technologies I've been working with recently:</p>
@@ -165,7 +163,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.png"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Shubham Soni</h2>;
+  const three = <h3 className="big-heading">An aspiring Software Engineer</h3>;
   const four = (
     <p>
-      I'm a software engineer specializing in building (and occasionally designing) exceptional
-      digital experiences. Currently, I'm focused on building accessible, human-centered products at{' '}
-      <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-        Upstatement
+      I'm an India based developer with a goal-driven creative mindset and passion for learning and
+      innovating who loves playing with web technologies and machine learning algorithms. I have
+      done my internship at{' '}
+      <a href="https://redwalltechnologies.com/" target="_blank" rel="noreferrer">
+        RedWall Technologies, Pvt. Ltd.
       </a>
-      .
     </p>
   );
   const five = (
